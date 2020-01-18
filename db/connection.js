17923154dd28eed9a9b1cb52156df48315f710e7
Kirsty McGlynn <kirsty.lmcg@gmail.com>
2020-01-18 21:28:45 +0000

    Remove verbose variable

--- db/connection.js
+++ db/connection.js
@@ -5,6 +5,4 @@ const dbConfig =
     ? { client: "pg", connection: process.env.DATABASE_URL }
     : require("../knexfile");
 
-const connection = knex(dbConfig);
-
-module.exports = connection;
+module.exports = knex(dbConfig);
